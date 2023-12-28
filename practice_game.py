0c204758b297ff8a70697f8985435e6c068862ad
HanYeyoung <hany2813@naver.com>
2023-12-27 21:28:18 -0600

    modify that charcter moves with mouse keys properly

--- practice_game.py
+++ practice_game.py
@@ -10,15 +10,15 @@ screen_height = 640
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Screen Title
-pygame.display.set_caption("POO")
+pygame.display.set_caption("POO GAME")
 
 # FPS
 clock = pygame.time.Clock()
 ############################################################
 
 # 1. User Game Initialization (Background, Game Image, Coordinate, Speed, Font, etc)
-
 background = pygame.image.load("/Users/yeyounghan/Desktop/pygame_basic/background.png")
+
 character = pygame.image.load("/Users/yeyounghan/Desktop/pygame_basic/character.png")
 character_size = character.get_rect().size
 character_width = character_size[0]
@@ -26,6 +26,7 @@ character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2)
 character_y_pos = screen_height - character_height
 
+# move location
 to_x = 0
 character_speed = 10
 
@@ -47,9 +48,9 @@ while running:
             running = False
 
         if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT:
                 to_x -= character_speed
-            elif event.type == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT:
                 to_x += character_speed
 
         if event.type == pygame.KEYUP:
@@ -57,7 +58,7 @@ while running:
                 to_x = 0
 
     # 3. Game Character Location
-    character_x_pos += to_x * dt
+    character_x_pos += to_x
 
     if character_x_pos < 0:
         character_x_pos = 0
